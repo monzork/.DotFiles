ee97722446f76f9734840f90db7babb6058e31cb
Marlon Alonzo <marlon.a@phonecheck.com>
2023-06-12 20:09:33 -0600

    null-ls with eslint

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,8 +1,11 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-    return
-end
+if not null_ls_status_ok then return end
 
+
+local conditional = function(fn)
+    local utils = require('null-ls.utils').make_conditional_utils()
+    return fn(utils)
+end
 local on_attach = function(_, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format({
@@ -11,14 +14,18 @@ local on_attach = function(_, bufnr)
                 return client.name == "null-ls"
             end
         })
-        print("File formatted")
     end, { desc = "Format current buffer with LSP" })
 end
 
 null_ls.setup({
     debug = false,
     sources = {
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint.with({
+            condition = function(utils)
+                return utils.root_has_file({ '.eslintrc' })
+            end
+        }),
         null_ls.builtins.formatting.prettierd,
     },
+    on_attach = on_attach
 })
