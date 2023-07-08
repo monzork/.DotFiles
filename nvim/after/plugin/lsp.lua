da9adddb17007ba921876d94861e8424a940e86e
Marlon Alonzo <malonzoareas@gmail.com>
2023-07-08 16:36:08 -0600

    added languages by default

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,11 +2,12 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'lua',
-})
+-- lsp.ensure_installed({
+--     'tsserver',
+--     'eslint',
+--     'lua',
+--     '
+-- })
 
 lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr }
